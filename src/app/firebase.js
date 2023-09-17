2de222194c1e128e745e21f321d1e26b3ec9c796
Abhishek Raman <beastonlineg@gmail.com>
2023-09-17 14:14:25 +0530

    firestore

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,13 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { getFips } from "crypto";
 import { initializeApp } from "firebase/app";
-
+import firebase from "firebase/app";
+import "firebase/auth";
+import "@firebase/firestore";
 
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
+import { Firestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBR_i5VIOkfN2jCKYcgz53MCp1Gd4MFlsE",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-'use client'; // Changed from 'use client' to 'use strict'
+'use client';
 
-import React, { useState } from "react"; // Added { useState }
+import React, { useState } from "react";
 import Router from "next/router";
-import firebase from "firebase/app";
+
 import "firebase/auth";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc,   } from "firebase/firestore";
+import "firebase/auth";
+import "@firebase/firestore";
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { getFirestore, Firestore } from "firebase/firestore";
 
 export default function Home() {
   const [items, setItems] = useState([
@@ -12,28 +19,35 @@ export default function Home() {
     { name: 'Movie', price: 7.95 },
     { name: 'candy', price: 3.95 },
   ]);
+  const [newItem, setNewItem] = useState({name: '',price: ''});
   const [total, setTotal] = useState(0);
 
-  //Add items to database
-  const addItem = async (e) => {
+ // Initialize Firestore and assign it to db
+
+ const db: Firestore = firebase.firestore();
+  const addItem = async (e:React.FormEvent) => {
     e.preventDefault()
+  
+  if(newItem.name!= '' && newItem.price! == '')
+  {
+      await addDoc(collection(db,"items"), {
+      name: newItem.name.trim(),
+      price: newItem.price,
+    })
   }
-  //Read items from the database
-
-  //Delete items from database
-
+};
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24">
       <div className="flex-col w-full z-10 max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1 className="text-4xl p-4 text-center">Expense Tracker</h1>
         <div className="mt-16 bg-slate-700 rounded-lg p-4">
           <form className="grid grid-cols-6 items-center text-black">
-            <input className="col-span-3 p-3 border" type="text" placeholder="Enter Item" />
-            <input className="col-span-2 p-3 border mx-3" type="number" placeholder="Enter $" />
-            <button className="text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl" type="submit">+</button>
+            <input value={newItem.name} onChange={(e) => setNewItem({...newItem,name: e.target.value})} className="col-span-3 p-3 border" type="text" placeholder="Enter Item" />
+            <input value={newItem.price} onChange={(e) => setNewItem({...newItem,price: e.target.value})} className="col-span-2 p-3 border mx-3" type="number" placeholder="Enter $" />
+            <button onClick={addItem} className="text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl" type="submit">+</button>
           </form>
           <ul>
-            {items.map((item, id) => ( // Changed 'key="id"' to 'key={id}'
+            {items.map((item, id) => (
               <li key={id} className="my-4 w-full flex justify-between bg-slate-950">
                 <div className="p-4 w-full flex justify-between">
                   <span className="capitalize">{item.name}</span>
@@ -43,7 +57,7 @@ export default function Home() {
               </li>
             ))}
           </ul>
-          {items.length < 1 ? '' : ( // Removed the extra parentheses
+          {items.length < 1 ? '' : (
             <div className="flex justify-between p-3">
               <span>Total</span>
               <span>${total}</span>
